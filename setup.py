5148731435229181731a314fd4d8f3e3c92cf65d
Shashank Chhoker <shashanksaad07@gmail.com>
2024-08-31 15:54:15 +0530

    Requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="US_visa",
+    version="0.0.0",
+    author="Shashank Chhoker",
+    author_email="shashanksaad07@gmail.com",
+    packages=find_packages()
+    
+    
+)
