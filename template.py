a388b4c6bf92d957556b24c0ccf3dfd544341a65
Shashank Chhoker <shashanksaad07@gmail.com>
2024-08-31 15:27:29 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+project_name = "US_visa"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    # Create directories if they don't exist
+    if not filepath.parent.exists():
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+    
+    # Create the file if it doesn't exist or if it's empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+    else:
+        print(f"file is already present at: {filepath}")
